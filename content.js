2adfd43928298bc52787aec34c0135647fe54e78
DevOpsDavid <david.frings.development@gmail.com>
2024-09-06 16:33:10 +0200

    feat: Added Uppercase support

--- content.js
+++ content.js
@@ -1,13 +1,9 @@
-// Funktion zum Ersetzen der Commits durch Bilder
 function replaceCommitTypes() {
-  // Hole alle Commit-Nachricht-Links
   const commitMessages = document.querySelectorAll('a[data-pjax="true"]');
   
   commitMessages.forEach(message => {
-    // Hole den Textinhalt des Links
-    const text = message.textContent.trim();
-    
-    // Ersetze Text durch Bilder
+    const text = message.textContent.trim().toLowerCase();
+
     if (text.startsWith('feat:')) {
       message.innerHTML = `<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqQXCfw2Ulfrfe1xG2NGkSe7FOnT0h9AEjcQ&s" alt="Feature" class="commit-icon"> ${text.replace('feat:', '')}`;
     } else if (text.startsWith('chore:')) {
@@ -18,13 +14,11 @@ function replaceCommitTypes() {
   });
 }
 
-// Event Listener, um sicherzustellen, dass das Skript nach dem Laden der Seite ausgeführt wird
 window.addEventListener('load', function() {
   console.log('Script loaded');
   replaceCommitTypes();
 });
 
-// MutationObserver, um Änderungen dynamisch zu überwachen
 const observer = new MutationObserver(function(mutations) {
   mutations.forEach(function(mutation) {
     if (mutation.addedNodes.length) {
